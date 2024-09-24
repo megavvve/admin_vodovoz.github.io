1291208ce56c9899773267ac96a2b049812d27b2
Sergei <aserka327@gmail.com>
2024-09-24 15:59:08 +0300

    FIX: bug #4

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"059e4e6d8ff6de39c29441c53e949bfb0bf179
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "235573809"
+    serviceWorkerVersion: "2382567795"
   }
 });
